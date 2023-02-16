6026ec6a1d33075bb8d04f6d90efdea5a9813e3b
rohit <rohitkwankhede@gmail.com>
2023-02-16 14:14:06 +0530

    oo

---
+++ R/example.R
@@ -0,0 +1,8 @@
+# Given a numeric vector, convert to strings, sort, and convert back to
+# numeric.
+lexical_sort <- function(x) {
+  as.numeric(sort(as.character(x)))
+}
+
+
+#snapshot_review("")
---
+++ tests/testthat/test-examplemodule.R
@@ -0,0 +1,18 @@
+# See ?testServer for more information
+testServer(exampleModuleServer, {
+  # Set initial value of a button#app$snapshot()
+  
+  # Input 'l_groups' was set, but doesn't have an input binding.
+  session$setInputs(o = "Ficus microcarpa nitida 'Green Gem' :: Indian Laurel Fig Tree 'Green Gem'")
+  #app$snapshot()
+  # Input 'l_marker_mouseover' was set, but doesn't have an input binding.
+  # Input 'l_marker_mouseout' was set, but doesn't have an input binding.
+  # Input 'l_marker_mouseover' was set, but doesn't have an input binding.
+  # Input 'l_marker_click' was set, but doesn't have an input binding.
+  # Input 'l_marker_mouseout' was set, but doesn't have an input binding.
+  #app$snapshot()
+  
+  expect_equal(input$o,"Ficus microcarpa nitida 'Green Gem' :: Indian Laurel Fig Tree 'Green Gem'")
+  session$setInputs(o = "Myoporum laetum :: Myoporum")
+  
+})
